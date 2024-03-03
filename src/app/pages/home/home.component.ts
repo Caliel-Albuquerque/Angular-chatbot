5202170003c8dc5dfdc4ff5b551ff7f73b65c7f3
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2024-03-03 18:15:52 -0300

    feat: add logic to go a new page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ArrowRightComponent } from '../../icons/arrow-right/arrow-right.component';
 import { SparkleComponent } from '../../icons/sparkle/sparkle.component';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -10,5 +12,9 @@ import { SparkleComponent } from '../../icons/sparkle/sparkle.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  openChat() {
+    this.router.navigate(["chat"])
+  }
 }
