a3c3ef59546b517bdf0166a8cff4bd251ab6b13f
Caliel-Albuquerque <75955275+Caliel-Albuquerque@users.noreply.github.com>
2024-03-06 13:53:14 -0300

    feat: chat response dialog

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -5,6 +5,8 @@ import { ChatSuggestionsComponent } from '../../components/chat-suggestions/chat
 import { CommonModule } from '@angular/common';
 import { Message } from '../../types/message.type';
 import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.component';
+import { MessageService } from '../../services/message.service';
+
 
 
 @Component({
@@ -16,23 +18,46 @@ import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.co
     CommonModule,
     ChatDialogComponent
   ],
+  providers: [
+
+    MessageService
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
 
-  constructor(private route: Router) {}
+  constructor(private route: Router, private service: MessageService) {}
 
   closeChat() {
     this.route.navigate([""])
   }
 
-  questions: Array<Message> = []
+  questions: Array<Message> = JSON.parse(localStorage.getItem("messages") ?? "[]")
+
+  updateLocalStorage() {
+    localStorage.setItem("message", JSON.stringify(this.questions))
+  }
 
   sendSuggestedQuestion(question: string) {
     this.questions.push({
       type: 'request',
       message: question
+    })
+
+    this.updateLocalStorage();
+    this.sendMessage(question);
+  }
+
+  sendMessage(message: string) {
+    this.service.sendData(message).subscribe({
+      next:(body) => {
+        this.questions.push({
+          type: 'response',
+          message: body.response
+        })
+        this.updateLocalStorage();
+      }
     })
   }
 }
